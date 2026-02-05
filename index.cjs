10ebf3bfa99597b1bf3994069a50ef17bf05d5e0
hajos8 <rabb.andras@gmail.com>
2026-02-05 09:07:13 +0100

    Add firebase config

--- index.cjs
+++ index.cjs
@@ -2,6 +2,9 @@ const express = require('express');
 const cors = require('cors');
 const firebase = require('firebase/app');
 
+//pnpm install express cors firebase
+//run command: node --env-file=.env .\index.cjs
+
 require('firebase/auth');
 require('firebase/database');
 
@@ -10,7 +13,14 @@ app = express();
 app.use(express.json());
 app.use(cors());
 
-const firebaseConfig = {};
+const firebaseConfig = {
+    apiKey: process.env.VITE_FIREBASE_apiKey,
+    authDomain: process.env.VITE_FIREBASE_authDomain,
+    projectId: process.env.VITE_FIREBASE_projectId,
+    storageBucket: process.env.VITE_FIREBASE_storageBucket,
+    messagingSenderId: process.env.VITE_FIREBASE_messagingSenderId,
+    appId: process.env.VITE_FIREBASE_appId
+};
 
 app.post('/api/inventory-check', (req, res) => {
     const { auth_id } = req.body;
@@ -23,4 +33,5 @@ app.post('/api/inventory-check', (req, res) => {
 const port = 3333;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
+    console.log('Firebase config:', firebaseConfig);
 })
